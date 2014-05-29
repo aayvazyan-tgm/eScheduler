34a577ba7d2dbe044098a23648b7c5b667d87a75
SEW <SEW@CT102770>
2014-05-29 17:51:35 +0200

    Implemented register()

--- src/escheduler/controller/RegisterController.java
+++ src/escheduler/controller/RegisterController.java
@@ -1,8 +1,75 @@
 package escheduler.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import escheduler.model.User;
+
+/**
+ * This class handles user registrations in the System.
+ * 
+ * @author Andreas Willinger
+ * @version 20140529.1
+ */
 public class RegisterController 
 {
-	public boolean register(String username, String password) {
-		return false;
+	/**
+	 * Creates a new User in the System, with login capabilities.
+	 * 
+	 * @param username The unique name of the new user
+	 * @param password The password of the new user
+	 * @return true, if creation was successful, false, if the user already exists or there was a DB failure.
+	 */
+	public boolean register(String username, String password) 
+	{
+		if(username == null || password == null)
+			return false;
+		
+		Logger lg=Logger.getLogger("Debug");
+		Session session = SessionManager.getInstance().getHibernateSession();
+		
+		if(session == null)
+			return false;
+		
+		// first, let's check if the user already exists
+		Query query = session.getNamedQuery("checkExistance")
+				.setString("username", username);
+			
+		List<User> results = (List<User>)query.list();
+			
+		if(results.size() > 0)
+				return false;
+		
+		// then, create a new transaction and actually save the user
+		Transaction tx = session.beginTransaction();
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		
+		try
+		{
+			session.save(user);
+			tx.commit();
+			
+			return true;
+		}
+		catch(RuntimeException re)
+		{
+			try
+			{
+				tx.rollback();
+			}
+			catch(RuntimeException re2)
+			{
+				lg.error("Commit/Rollback fehlgeschlagen (Exception). ", re2);
+			}
+			
+			return false;
+		}
 	}
 }
