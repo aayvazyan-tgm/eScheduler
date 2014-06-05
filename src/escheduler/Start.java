7b4e19efcf1c67e83db5e3028536519b1c7303f4
Andreas Willinger <awi95@gmx.at>
2014-06-05 14:01:14 +0200

    General changes
    - Start class re-activated, used to properly configure log4j
    - Updated build.xml: added doc, clean-doc (for javadoc generation)
    targets, added copying of log4j.xml file

--- src/escheduler/Start.java
+++ src/escheduler/Start.java
@@ -7,18 +7,16 @@ import org.apache.log4j.*;
  * The main class, that executes the program and Configures the Logger.
  * 
  * @author Ari Ayvazyan
- * @version 30.05.2014
+ * @version 05.06.2014
  */
 public class Start
-	//implements ServletContextListener
+	implements ServletContextListener
 {	
 	/**
 	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
 	 */
 	public void contextInitialized(ServletContextEvent event)
 	{
-		ConsoleAppender ca = new ConsoleAppender(); //creates a new Console appender for Log4J
-		BasicConfigurator.configure(ca); //assign the appender to the Log4J configuration
 		Logger lg = Logger.getLogger("Debug");
 		lg.log(Level.INFO, "Der Betrieb wird aufgenommen");
 	}
